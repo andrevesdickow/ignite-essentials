e2040a403a58868c175f4dff6a526bb4e7bc798d
Andrêves Dickow <andrevesdickow@MacBook-Air-de-Andreves.local>
2022-01-28 14:32:06 -0300

    - ajustes na responsividade ao criar usuário. - criado navegação entre as páginas.

--- ReactJS/Chapter-4/dashgo/src/pages/users/index.tsx
+++ ReactJS/Chapter-4/dashgo/src/pages/users/index.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Text, Tbody, Th, Thead, Tr, Td, useBreakpointValue } from "@chakra-ui/react"
 import { RiAddLine, RiPencilLine } from "react-icons/ri"
+import Link from 'next/link'
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Text, Tbody, Th, Thead, Tr, Td, useBreakpointValue } from "@chakra-ui/react"
 import { Header } from "../../components/Header"
 import { Pagination } from "../../components/Pagination"
 import { Sidebar } from "../../components/Sidebar"
@@ -26,15 +27,17 @@ export default function ListUsers() {
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-            >
-              Criar novo
-            </Button>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
           <Table
